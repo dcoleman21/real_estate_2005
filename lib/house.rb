751273331faf2a36178b5fabeaa3875905d3d551
Dani Coleman <danicoleman00@gmail.com>
2020-05-13 10:53:56 -0600

    Create house class with methods

---
+++ lib/house.rb
@@ -0,0 +1,22 @@
+class House
+  attr_reader :price,
+              :address,
+              :rooms
+  def initialize(price, address)
+    @price = price
+    @address = address
+    @rooms = []
+  end
+
+  def add_room(room)
+    @rooms << room
+  end
+
+  def above_market_average?
+    if @price > $500000
+      true
+    else
+      false
+    end
+  end
+end
